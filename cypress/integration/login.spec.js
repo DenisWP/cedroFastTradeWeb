7d44f376bb992cdc724bb607561e148de93eb48d
DenisWP <denis.dabliu@gmail.com>
2021-06-21 13:12:28 -0300

    Atualização do formulário

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -16,7 +16,13 @@ describe('FastTrade Login', () => {
         cy.get('[id="phone"]').type('31985856666');
         cy.get('[formcontrolname="birth_date"]').type('20/08/1990');
 
-        cy.get('[id="btn-form-valid-next"]').click();        
+        cy.get('[id="btn-form-valid-next"]').click();     
+        
+        cy.get('[id="zip_code"]').type('35455970');
+        cy.get('[id="number"]').type('30');
+
+        cy.get('[id="check-box-accept-terms-input"]').click();
+        cy.get('[id="btn-form-valid-next""]').click();      
 
     });
 
